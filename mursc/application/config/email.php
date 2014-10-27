ed3ed611837ca62e3c9d97da383325b23c970822
hiruko3 <romain3.verger3@gmail.com>
2014-10-27 15:54:37 +0100

    email.php file configuration added

---
+++ mursc/application/config/email.php
@@ -0,0 +1,18 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------------
+| Configuration EMAIL
+| -------------------------------------------------------------------------
+*/
+ 
+$config['protocol'] = "smtp";
+$config['smtp_host'] = "smtp.gmail.com";
+$config['smtp_port'] = "587";
+$config['smtp_user'] = "serveurtestromain@gmail.com";
+$config['smtp_pass'] = "serveur_test_password3!";
+$config['smtp_timeout'] = "5";
+$config['smtp_crypto'] = "tls";
+$config['crlf'] = "\r\n";
+$config['newline'] = "\r\n";
+$config['mailtype'] = "html";
+$config['charset'] = "utf-8"; 
