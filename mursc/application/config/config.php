7a07acc1b94eba452784c863bd9be060c354b001
hiruko3 <romain3.verger3@gmail.com>
2014-10-27 15:55:25 +0100

    restore deposit

--- mursc/application/config/config.php
+++ mursc/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://127.0.0.1/cdp_site/mursc/';
+$config['base_url']	= 'http://127.0.0.1/CDP/Project/ProjectCDP/mursc/';
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'HwS6bPeg079U3O5j5ex232XH27lZ9RTb';
 
 /*
 |--------------------------------------------------------------------------
