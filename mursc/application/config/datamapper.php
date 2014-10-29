9dfce248ecf999d9444d0cb171aeb8dc82146807
solene <fsjolly@yahoo.fr>
2014-10-29 15:49:12 +0100

    config datamapper

--- mursc/application/config/datamapper.php
+++ mursc/application/config/datamapper.php
@@ -1,12 +1,10 @@
 <?php	if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-
 /**
- * Data Mapper Configuration
- *
- * Global configuration settings that apply to all DataMapped models.
- */
-
-$config['prefix'] = '';
+* Data Mapper Configuration
+*
+* Global configuration settings that apply to all DataMapped models.
+*/
+$config['prefix'] = 'mursc_';
 $config['join_prefix'] = '';
 $config['error_prefix'] = '<p>';
 $config['error_suffix'] = '</p>';
@@ -28,6 +26,5 @@ $config['db_params'] = '';
 // $config['production_cache'] = 'datamapper/cache';
 $config['extensions_path'] = 'datamapper';
 $config['extensions'] = array();
-
 /* End of file datamapper.php */
 /* Location: ./application/config/datamapper.php */
